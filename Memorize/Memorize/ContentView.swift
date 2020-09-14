5258a9c3ecb0661bc64c2ec8a3862487b682b26e
CrowFea <wendaoliu40@outlook.com>
2020-09-14 21:37:35 +0800

    feat：添加初始文件

---
+++ Memorize/Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by liusiyuan on 2020/9/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
